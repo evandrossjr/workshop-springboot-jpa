190ec3d21354d19d58db60ddaef1da10c9fdd467
Evandro Sacramento <evandro-dev@outlook.com>
2025-02-20 23:25:46 -0300

    Fix H2

--- src/main/java/com/webcourse/course/CourseApplication.java
+++ src/main/java/com/webcourse/course/CourseApplication.java
@@ -2,12 +2,24 @@ package com.webcourse.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CourseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
-	}
+		ApplicationContext applicationContext = SpringApplication.run(CourseApplication.class, args);
+        Environment environment = applicationContext.getEnvironment();
+        String[] activeProfiles = environment.getActiveProfiles();
 
-}
+        System.out.println("Active Profiles:");
+        for (String profile : activeProfiles) {
+            System.out.println(profile);
+        }
+
+        System.out.println("Database URL: " + environment.getProperty("spring.datasource.url"));
+        System.out.println("Database Driver: " + environment.getProperty("spring.datasource.driver-class-name"));
+        System.out.println("Database Username: " + environment.getProperty("spring.datasource.username"));
+	} 
+}	
