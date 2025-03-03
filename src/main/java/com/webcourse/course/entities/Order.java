9c51c97e21056e58054b0eb5c095de565c2ac2a0
Evandro Sacramento <evandro-dev@outlook.com>
2025-03-03 19:41:15 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/webcourse/course/entities/Order.java
+++ src/main/java/com/webcourse/course/entities/Order.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +27,8 @@ public class Order implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment; 
 	
 	@ManyToOne
---
+++ src/main/java/com/webcourse/course/repositories/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.webcourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webcourse.course.entities.Order;
+
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+
+}
