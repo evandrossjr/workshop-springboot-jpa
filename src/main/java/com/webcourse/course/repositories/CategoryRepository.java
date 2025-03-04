d2df20d7ffc0d817ce96b838fef5a8842babd3f7
Evandro Sacramento <evandro-dev@outlook.com>
2025-03-04 10:57:16 -0300

    Category

---
+++ src/main/java/com/webcourse/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.webcourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webcourse.course.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+
+}
---
+++ src/main/java/com/webcourse/course/services/CategoryService.java
@@ -0,0 +1,31 @@
+package com.webcourse.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webcourse.course.entities.Category;
+import com.webcourse.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
