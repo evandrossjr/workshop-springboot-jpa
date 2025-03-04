04bc40296ffb1b3555b3371fc7f84ec019a75ebc
Evandro Sacramento <evandro-dev@outlook.com>
2025-03-04 15:00:36 -0300

    OrderItem, many-to-many association with extra attributes part 2

--- src/main/java/com/webcourse/course/entities/Order.java
+++ src/main/java/com/webcourse/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.webcourse.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.webcourse.course.entities.enums.OrderStatus;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -35,6 +38,12 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	
+	//adicinonado após criar as classes OrderItemPk e OrderItem
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Order() {
 		
@@ -84,6 +93,11 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
+	
+	public Set<OrderItem> getItems(){
+		return items;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
---
+++ src/main/java/com/webcourse/course/repositories/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.webcourse.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webcourse.course.entities.OrderItem;
+import com.webcourse.course.entities.pk.OrderItemPK;
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+	
+
+}
+
